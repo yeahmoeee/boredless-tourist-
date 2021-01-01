df45fa8c741b9731b1d5120cc2270932c2782de0
codecademy <ccuser@codecademy.com>
2021-01-01 22:08:57 +0000

    Added test objects

---
+++ script.py
@@ -0,0 +1,10 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination
+
+print(get_destination_index("Los Angeles, USA"))
+
